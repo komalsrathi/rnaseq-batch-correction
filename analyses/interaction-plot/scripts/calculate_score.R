e01e5395b9e33b5305665edfeec6f8269cfa35f2
Alex Sickler <sicklera@chop.edu>
2020-05-28 10:34:51 -0400

    remove Kids_First_Participant_ID from samples_with_disease funciton

--- analyses/interaction-plot/scripts/calculate_score.R
+++ analyses/interaction-plot/scripts/calculate_score.R
@@ -9,7 +9,7 @@ samples_with_disease <- function(meta_df, sample_df, disease_id) {
     dplyr::filter(Kids_First_Biospecimen_ID %in% sample_df$Kids_First_Biospecimen_ID) %>%
     dplyr::filter(tolower(disease_id) == "all" |
       tolower(short_histology) == tolower(disease_id)) %>%
-    dplyr::select(Kids_First_Participant_ID, Kids_First_Biospecimen_ID)
+    dplyr::select(Kids_First_Biospecimen_ID)
 
   samples <- disease_df$Kids_First_Biospecimen_ID
   return(samples)
