0bc55a3034c3e9cdb1189a0d9c14a33410b7a6ae
komalsrathi <komal.rathi@gmail.com>
2020-11-02 17:59:50 -0500

    add updated analysis

--- analyses/rnaseq-batch-correct/pbta-hgat-dx-upload.sh
+++ analyses/rnaseq-batch-correct/pbta-hgat-dx-upload.sh
@@ -7,5 +7,5 @@ aws s3 --profile saml cp output/pbta-hgat-dx-gene-expression-rsem-tpm-corrected.
 aws s3 --profile saml cp output/pbta-hgat-dx-gene-expression-rsem-fpkm-corrected.rds s3://d3b-bix-dev-data-bucket/hgg-dmg-integration/merged_ngs_files/rna-seq/batch-corrected/
 
 # combined files
-aws s3 --profile saml cp output/pbta-gene-expression-rsem-tpm-collapsed.combined.rds s3://d3b-bix-dev-data-bucket/hgg-dmg-integration/merged_ngs_files/rna-seq
-aws s3 --profile saml cp output/pbta-gene-expression-rsem-fpkm-collapsed.combined.rds s3://d3b-bix-dev-data-bucket/hgg-dmg-integration/merged_ngs_files/rna-seq
+aws s3 --profile saml cp output/pbta-gene-expression-rsem-tpm-collapsed.combined.rds s3://d3b-bix-dev-data-bucket/hgg-dmg-integration/merged_ngs_files/rna-seq/
+aws s3 --profile saml cp output/pbta-gene-expression-rsem-fpkm-collapsed.combined.rds s3://d3b-bix-dev-data-bucket/hgg-dmg-integration/merged_ngs_files/rna-seq/
