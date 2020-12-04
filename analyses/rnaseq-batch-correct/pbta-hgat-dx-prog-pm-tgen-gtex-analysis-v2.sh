0b2989070cd43d2155e83468f35e14de9bacc285
komalsrathi <komal.rathi@gmail.com>
2020-12-04 11:58:03 -0500

    remove fpkm output files

--- analyses/rnaseq-batch-correct/pbta-hgat-dx-prog-pm-tgen-gtex-analysis-v2.sh
+++ analyses/rnaseq-batch-correct/pbta-hgat-dx-prog-pm-tgen-gtex-analysis-v2.sh
@@ -19,11 +19,6 @@ Rscript code/02-combine-matrices.R \
 --matrices "input/pbta-gene-expression-rsem-tpm-collapsed.stranded.rds, input/pbta-gene-expression-rsem-tpm-collapsed.polya.rds, input/tgen-brain-normals-gene-expression-rsem-tpm.collapsed.polya.rds, input/gtex-brain-normals-gene-expression-rsem-tpm.collapsed.polya.rds" \
 --outfile "pbta-tgen-gtex-gene-expression-rsem-tpm-collapsed.combined.rds"
 
-# combine fpkm matrices
-Rscript code/02-combine-matrices.R \
---matrices "input/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds, input/pbta-gene-expression-rsem-fpkm-collapsed.polya.rds, input/tgen-brain-normals-gene-expression-rsem-fpkm.collapsed.polya.rds, input/gtex-brain-normals-gene-expression-rsem-fpkm.collapsed.polya.rds" \
---outfile "pbta-tgen-gtex-gene-expression-rsem-fpkm-collapsed.combined.rds"
-
 # combine expected count matrices
 Rscript code/02-combine-matrices.R \
 --matrices "input/pbta-gene-counts-rsem-expected_count-collapsed.polya.rds, input/pbta-gene-counts-rsem-expected_count-collapsed.stranded.rds, input/tgen-brain-normals-gene-counts-rsem-expected_count.collapsed.polya.rds, input/gtex-brain-normals-gene-counts-rsem-expected_count.collapsed.polya.rds" \
